c5e4060dd0f470e46bf8eb58058ffa3b900ef9c0
Reformas2001 <reformas.madrid2001@yahoo.es>
2025-08-28 17:16:29 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menu-toggle");
+  const navMenu = document.querySelector(".nav ul");
+
+  menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+  });
+});
